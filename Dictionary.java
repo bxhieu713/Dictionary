27deb94590cff04a14bf353210d09719c4b501a4
Bhieu <bhieu332001@gmail.com>
2020-10-27 21:21:30 +0700

    update

---
+++ Dictionary.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dictionary {
+
+    public ArrayList<Word> dictionary = new ArrayList<Word>();
+
+    public Dictionary() {
+    }
+
+    public void addWord(Word word) {
+        this.dictionary.add(word);
+    }
+
+    public void addWord(int index, Word word) {
+        this.dictionary.add(index, word);
+    }
+
+    public Word getWord(int i) {
+        return dictionary.get(i);
+    }
+
+    public void removeWord(int i) {
+        this.dictionary.remove(i);
+    }
+
+    public void changeWord(int i, Word word) {
+        this.dictionary.remove(i);
+        this.addWord(i, word);
+    }
+
+    public void getList(ArrayList<Word> _list) {
+        this.dictionary = _list;
+    }
+
+    public void printList() {
+        System.out.printf("%-5s|%-20s|%-20s\n", "No", "English", "Vietnamese");
+        for (int i = 0; i < dictionary.size(); i++) {
+            System.out.printf("%-5d", i + 1);
+            dictionary.get(i).printWord();
+        }
+    }
+}
